7c6d0086828f1dd63855cfe1eb0d55cc329d7b29
Futa Hirakoba <korosuke613613@gmail.com>
2019-04-19 07:56:27 +0900

    リンク集を追加

--- src/components/layouts/default.vue
+++ src/components/layouts/default.vue
@@ -1,18 +1,21 @@
 <template>
-    <div>
-        <Banner />
-        <NasBeeeno />
-        <nuxt/>
-    </div>
+  <div>
+    <Banner/>
+    <NasBeeeno/>
+    <nuxt/>
+    <Links/>
+  </div>
 </template>
 
 <script>
-    import Banner from '../molcules/Banner.vue'
-    import NasBeeeno from '../atoms/NasBeeeno.vue'
-    export default {
-        components:{
-            Banner,
-            NasBeeeno
-        }
-    }
+import Banner from "../molcules/Banner.vue";
+import NasBeeeno from "../atoms/NasBeeeno.vue";
+import Links from "../molcules/Links.vue";
+export default {
+  components: {
+    Banner,
+    NasBeeeno,
+    Links
+  }
+};
 </script>
---
+++ src/components/molcules/Links.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>リンク集</h2>
+    <ul>
+      <li>
+        <nuxt-link to="/from">出身</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/tax">税金計算</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
