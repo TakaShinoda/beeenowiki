0b57bd9c6012797fb99ac8c1e66508ab18d2b4d9
Futa Hirakoba <korosuke613613@gmail.com>
2019-04-19 03:51:08 +0900

    fix import

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Home from '../pages/Home.vue'
-import From from '../pages/From.vue'
-import taxIncluded from '../pages/taxIncluded.vue'
+import Home from '../components/pages/Home.vue'
+import From from '../components/pages/From.vue'
+import taxIncluded from '../components/pages/taxIncluded.vue'
 
 Vue.use(VueRouter)
 
