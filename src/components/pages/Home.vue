d6cd13dc9edcd8f5478e2bc951bd2f02cf5878ba
Futa Hirakoba <korosuke613613@gmail.com>
2019-04-19 03:51:44 +0900

    修正

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <p><div>ここはBeeenoの情報をまとめるwikiです。</div></p>
-    <router-link to="/about">Go to About page.</router-link>
+    ここはBeeenoの情報をまとめるwikiです。
   </div>
 </template>
 
