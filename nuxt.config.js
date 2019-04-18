cea5a54c4f3eaeed3a36274a41095bd661ce74fc
Futa Hirakoba <korosuke613613@gmail.com>
2019-04-19 08:10:56 +0900

    og:imageを設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,9 +15,10 @@ export default {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { property: 'og:title', content: 'Beeenowiki' },
+            { hid: 'og:title', property: 'og:title', content: 'Beeenowiki' },
+            { hid: 'og:image', property: 'og:image', content: 'https://github.com/TakaShinoda/beeenowiki/raw/master/src/img/beeno1.jpg' },
             // hidは一意の識別子として使用されます。`vmid` は動作しないので使わないでください
-            { hid: 'description', name: 'description', content: 'Beeeno information' }
+            { hid: 'description', name: 'description', content: 'Beeeno information' },
         ]
     },
     ...routerBase
