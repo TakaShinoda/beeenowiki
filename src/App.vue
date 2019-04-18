1aa6e2b82cd5c672052476bc7fe991ea07ee81aa
Futa Hirakoba <korosuke613613@gmail.com>
2019-04-19 03:45:27 +0900

    createNasBeeeno

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
     <div>
         <Banner />
-        <p>
-            <img src="./img/beeno1.jpg" alt="ビーノのイラスト" title="ナスビーノ" width="300">
-        </p>
+        <NasBeeeno />
         <router-view></router-view>
-
-        <li></li>
     </div>
 </template>
 
 <script>
     import Banner from './components/molcules/Banner.vue'
+    import NasBeeeno from './components/atoms/NasBeeeno.vue'
     export default {
         components:{
-            Banner
+            Banner,
+            NasBeeeno
         }
     }
 </script>
---
+++ src/components/atoms/NasBeeeno.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>
+      <img src="../../img/beeno1.jpg" alt="ビーノのイラスト" title="ナスビーノ" width="300">
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
