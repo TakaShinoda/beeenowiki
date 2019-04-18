1ebd0226b475939369ff13708dcf98bb1cb35d99
Futa Hirakoba <korosuke613613@gmail.com>
2019-04-19 08:05:19 +0900

    set og:title

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,13 +10,14 @@ export default {
         pages: 'src/components/pages',
     },
     head: {
+        title: 'Beeenowiki',
         titleTemplate: '%s - Beeenowiki',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-
+            { property: 'og:title', content: 'Beeenowiki' },
             // hidは一意の識別子として使用されます。`vmid` は動作しないので使わないでください
-            { hid: 'description', name: 'description', content: 'Meta description' }
+            { hid: 'description', name: 'description', content: 'Beeeno information' }
         ]
     },
     ...routerBase
