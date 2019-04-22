7a0b0adf5d3c3a25de3c2e74010a5fbf8c295b26
TakaShinoda+circleci <TakaShinoda+circleci@users.noreply.github.com>
2019-04-22 09:18:56 +0000

    Updates

--- _nuxt/47432a292ae4524142ca.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{144:function(e,t,n){"use strict";n.r(t);var l={layout:"default",head:function(){return{title:"Beeenowiki",titleTemplate:""}}},o=n(5),component=Object(o.a)(l,function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("ここはBeeenoの情報をまとめるwikiです。")])},[],!1,null,null,null);t.default=component.exports}}]);
