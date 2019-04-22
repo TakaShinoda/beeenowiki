52a3268a5a9da3955be31f79fa771e902f78210c
Futa Hirakoba <korosuke613613@gmail.com>
2019-04-23 00:09:45 +0900

    ⌬を出力するように追加

--- src/components/pages/benzen.vue
+++ src/components/pages/benzen.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <div>
-    <p>あなたの残業時間は？</p>
+      <p>あなたの残業時間は？</p>
       <input v-model.number="overtime" type="number">時間
-      <p>ベンゼン環 {{benzenIncluded}} 個獲得です！</p>
+      <p><big>ベンゼン環 {{benzenIncluded}} 個獲得です！</big></p>
+      <p><font size="7"><big>{{printBenzen}}</big></font></p>
     </div>
   </div>
 </template>
@@ -12,11 +13,19 @@
 export default {
   layout: "default",
   data() {
-    return {overtime: 10};
+    return { overtime: 10 };
   },
   computed: {
     benzenIncluded: function() {
-      return this.overtime / 10 ;
+      return this.overtime / 10;
+    },
+    printBenzen: function() {
+      const times = Math.floor(this.overtime / 10);
+      let benzen = "";
+      for (let i = 0; i < times; i++) {
+        benzen += "⌬";
+      }
+      return benzen;
     }
   },
   head() {
