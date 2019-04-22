53d77e9b97192c238e2734a928f8fba619b4228b
Your Name <qqgm3rk9k@gmail.com>
2019-04-22 18:18:33 +0900

    ベンゼン環を追加

---
+++ src/components/pages/benzen.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>
+      <input v-model.number="overtime" type="number">時間
+      <p>ベンゼン環 {{benzenIncluded}} 個獲得です！</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "default",
+  data() {
+    return { overtime: 100};
+  },
+  computed: {
+    benzenIncluded: function() {
+      return this.overtime / 10 ;
+    }
+  },
+  head() {
+    return {
+      title: "tax"
+    };
+  }
+};
+</script>
