6393f1ce0f4f52019ebc2d6b6a5c00952bf59bf5
Futa Hirakoba <korosuke613613@gmail.com>
2019-04-23 18:18:01 +0900

    searchconsoleに登録

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,6 +32,7 @@ export default {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { name: "google-site-verification", content: "V65tkfTzyTOAIVOF3kEq5KCkGYaNpG6fTLj8Imi6S-0" },
             { hid: 'og:title', property: 'og:title', content: 'Beeenowiki' },
             { hid: 'og:image', property: 'og:image', content: 'https://github.com/TakaShinoda/beeenowiki/raw/master/src/img/beeno1.jpg' },
             { hid: 'og:image:width', property: 'og:image:width', content: '1029' },
