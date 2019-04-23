de4387844d52830e58ce067558bdb402dbbbcbf9
Futa Hirakoba <korosuke613613@gmail.com>
2019-04-23 17:04:00 +0900

    過労死ライン計算機能を追加

--- src/components/pages/benzen.vue
+++ src/components/pages/benzen.vue
@@ -3,8 +3,16 @@
     <div>
       <p>あなたの残業時間は？</p>
       <input v-model.number="overtime" type="number">時間
-      <p><big>ベンゼン環 {{benzenIncluded}} 個獲得です！</big></p>
-      <p><font size="7"><big>{{printBenzen}}</big></font></p>
+      <p>
+        <big>ベンゼン環 {{benzenIncluded}} 個獲得です！</big>
+        <br>
+        {{isKaroshi}}
+      </p>
+      <p>
+        <font size="7">
+          <big>{{printBenzen}}</big>
+        </font>
+      </p>
     </div>
   </div>
 </template>
@@ -26,6 +34,16 @@ export default {
         benzen += "⌬";
       }
       return benzen;
+    },
+    isKaroshi: function() {
+      let tmp = "";
+      if (this.overtime >= 100) {
+        tmp = "過労死しました。";
+      } else if (this.overtime >= 80) {
+        tmp = "過労死ラインを超えました。";
+      }
+
+      return tmp;
     }
   },
   head() {
