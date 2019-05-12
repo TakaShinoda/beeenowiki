80aeca9e5ad16913dc0d971c97ea5247de06a011
Takahiro Shinoda <qqgm3rk9k@gmail.com>
2019-05-12 22:09:41 +0900

    出身にアニメーション追加

--- src/components/pages/From.vue
+++ src/components/pages/From.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <label>
-      <input type = "checkbox" v-model="myCheck">
-      Beeenoは修羅の国出身ですか？{{myCheck}}
+      <input type = "checkbox" v-model="isOk">
+      Beeenoは修羅の国出身ですか？
     </label>
+    <transition>
+      <p v-if="isOK">張っ倒すぞ！！</p>
+		</transition>
   </div>
 </template>
 
@@ -12,7 +15,7 @@ export default {
   layout: "default",
   data() {
     return {
-      myCheck: false
+      isOK: false
     } 
   },
   head() {
@@ -22,3 +25,15 @@ export default {
   }
 };
 </script>
+
+<style>
+	/* 現れている最中と、消えている最中は、0.5秒 */
+	.v-enter-active, .v-leave-active {
+		transition: 0.5s;
+	}
+	/* 現れる前の状態と、消えたときの状態は、透明度0で、下へ20移動*/
+	.v-enter, .v-leave-to {
+		opacity: 0;
+		transform: translateY(20px)
+	}
+</style>
