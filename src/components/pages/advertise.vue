61c8dc0562a7d4646c8f44113d8059aedf005126
Takahiro Shinoda <qqgm3rk9k@gmail.com>
2019-06-08 19:45:55 +0900

    ミュージカルのリンク追加

--- src/components/pages/advertise.vue
+++ src/components/pages/advertise.vue
@@ -11,7 +11,7 @@
 export default {
   layout: "default",
   data() {
-    return { content1: '9/21,22名古屋でミュージカル出ます。<a href="https://www.google.com/">詳しくはココ</a>'};
+    return { content1: '9/21,22名古屋でミュージカル出ます。<a href="https://commonbeat.org/project/musical-acb/52nd/">詳しくはココ</a>'};
   },
   head() {
     return {
